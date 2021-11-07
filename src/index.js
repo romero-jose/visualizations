4092d5f09da67ff7ad42c5abbefda1d768fd8351
romero-jose <jose.romero@ing.uchile.cl>
2021-11-07 00:24:31 -0300

    Refactors node and arrow into functions

--- src/index.js
+++ src/index.js
@@ -4,62 +4,90 @@ const OFFSET = 14;
 const WIDTH = 8;
 const HEIGHT = 5;
 const ARROW_WIDTH = 1;
+const LINE_WIDTH = 0.2;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+let scene, camera, renderer;
+let root;
 
-window.addEventListener('resize', onWindowResize);
+function init() {
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x050505);
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set(0, 0, 100);
-camera.lookAt(0, 0, 0);
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 5000);
+    camera.position.set(0, 0, 100);
+    scene.add(camera);
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xefefef);
+    root = new THREE.Group();
 
-function add_nodes() {
+    const l = link();
+    root.add(l);
+    scene.add(root);
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById( 'container' ).appendChild( renderer.domElement );
+
+    window.addEventListener('resize', onWindowResize);
+    renderer.render(scene, camera);
+}
+
+function link() {
+    const link = new THREE.Group();
+    link.add(node());
+    link.add(arrow());
+    return link;
+}
+
+function node() {
     const geometry = new THREE.PlaneGeometry(WIDTH, HEIGHT);
-    const material = new THREE.MeshBasicMaterial({ color: 0x808080 });
+    const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
     const node = new THREE.Mesh(geometry, material);
-
-    for (let i = 0; i < 10; ++i) {
-        const cpy = node.clone()
-        cpy.position.set(i * OFFSET, 0, 0);
-        scene.add(cpy);
-    }
+    node.position.set(0, 0, 0);
+    return node;
 }
 
-function add_arrows() {
-    const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-    const points = [];
-    const x0 = WIDTH / 4;
-    const x1 = OFFSET - WIDTH / 4;
-    const y = ARROW_WIDTH / 2;
-    points.push(new THREE.Vector3(x0, 0, 0));
-    points.push(new THREE.Vector3(x1 - y, 0, 0));
-    points.push(new THREE.Vector3(x1 - y, y, 0));
-    points.push(new THREE.Vector3(x1, 0, 0));
-    points.push(new THREE.Vector3(x1 - y, -y, 0));
-    points.push(new THREE.Vector3(x1 - y, 0, 0));
+function arrow() {
+    const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
 
-    const geometry = new THREE.BufferGeometry().setFromPoints(points);
-    const line = new THREE.Line(geometry, material);
+    const plane_geometry = new THREE.PlaneGeometry(OFFSET - WIDTH / 2, LINE_WIDTH);
+    const plane = new THREE.Mesh(plane_geometry, material);
+    plane.position.set(OFFSET / 2, 0, 0);
 
-    for (let i = 0; i < 10; ++i) {
-        const cpy = line.clone()
-        cpy.position.set(i * OFFSET, 0, 0);
-        scene.add(cpy);
-    }
+    const length = OFFSET;
+    const arrow_length = ARROW_WIDTH;
+    const arrow_width = ARROW_WIDTH;
+    const width = LINE_WIDTH;
+    const tail_length = length - arrow_length;
+
+    const head_geometry = new THREE.BufferGeometry();
+    const vertices = new Float32Array( [
+        0, width / 2, 0,
+        0, -width / 2, 0,
+        tail_length, -width / 2, 0,
+
+        tail_length, -width / 2, 0,
+        tail_length, width / 2, 0,
+        0, width / 2, 0,
+
+        tail_length, arrow_width / 2, 0,
+        tail_length, -arrow_width / 2, 0,
+        length, 0, 0,
+    ])
+    head_geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    const mesh = new THREE.Mesh(head_geometry, material);
+
+    return mesh;
 }
-
-add_nodes()
-add_arrows()
-renderer.render(scene, camera);
 
 function onWindowResize() {
+
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.render(scene, camera);
+
 }
+
+init();
